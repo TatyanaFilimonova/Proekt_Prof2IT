50c1096dad69883102befe4bad64a36b32e9f2ac
TatyanaFilimonova <tatyana0377@gmail.com>
2023-05-06 01:21:47 +0200

    long movies

---
+++ get_long_movies.py
@@ -0,0 +1,19 @@
+# 3. Отримайте назви всіх фільмів, які тривають понад 2 години.
+from pyspark.sql.functions import col
+
+
+def get_long_movies(title_df, output_file):
+    # фільтруємо фільми, які тривають понад duration_threshold годин
+    title_df_filtered = title_df.filter(col("ordering") > 2)
+
+    # вибираємо назви фільмів та їх тривалість
+    title_df_result = title_df_filtered.select(col("title"), col("ordering"))
+
+    # зберігаємо результат у csv файлі
+    title_df_result.write.format("csv").mode("overwrite").option("header", "true").save(output_file)
+
+    # виведемо результат
+    title_df_result.show()
+
+    # повертаємо результат
+    return title_df_result
